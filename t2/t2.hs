7e8752e9b9c09bb93db39072a9cc5687f4f96e21
pedrobms <pedro.bilar.montero@gmail.com>
2019-04-12 19:24:34 -0300

    novas cores e case 2 implentados

--- t2/t2.hs
+++ t2/t2.hs
@@ -12,23 +12,52 @@ rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
 
 greenPalette :: Int -> Int-> [(Int,Int,Int)]
 greenPalette c l = [(0,80+i*10+j*2,0) | i <- [0..c], j <- [0..l]]
+
+redPallete :: Int -> [(Int, Int, Int)]
+redPallete c = [(80+i*10,0,0) | i <- [0..c]]
+
+bluePallete :: Int -> [(Int, Int, Int)]
+bluePallete c = [(0,0,80+i*10) | i <- [0..c]]
+
+yellowPallete :: Int -> [(Int, Int, Int)]
+yellowPallete c = [(80+i*10, 80+i*10, 0) | i <- [0..c]]
+
+orangePallete :: Int -> [(Int, Int, Int)]
+orangePallete c = [(60+i*10, 30+i*5, 0) | i <- [0..c]]
+
 ------------------------
 -- Geração de figuras --
 ------------------------
+-- Gera lista representando retângulos de acordo com quantidade de linhas, colunas, espaçamento e dimensões dos retângulos
 genRect :: Int -> Int -> [Rect]
 genRect c l = [((x*(w+gap), y*(h+gap)), w, h) | x <- [0..fromIntegral(c-1)], y <- [0..fromIntegral(l-1)]]
   where (w, h) = (50, 25)
         gap    = 5
 
-----------------
--- String SVG --
-----------------
+-- Conversao de graus para radianos
+degreeToRad :: Float -> Float -> Float
+degreeToRad x n = ((x*360)/n)*(pi/180)
 
+-- Gera lista representando circunferência de círculos de acordo com quantidade de círculos, raio dos círculos e raio da circunferência
+genCircle1 :: Int -> [Circle]
+genCircle1 n = [(((cos(degreeToRad x (fromIntegral n))*rC+300), (sin(degreeToRad x (fromIntegral n))*rC+300)), r) | x <- [0..fromIntegral(n-1)]]
+  where r   = 20
+        rC = 250
+
+----------------
+-- Strings SVG --
+----------------
 -- Gera string representando retângulo SVG 
 -- dadas coordenadas e dimensoes do retângulo e uma string com atributos de estilo
 svgRect :: Rect -> String -> String 
 svgRect ((x,y),w,h) style = 
-  printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
+  printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' stroke='black' style='%s' />\n" x y w h style
+
+-- Gera string representando círculos SVG 
+-- dadas coordenadas e raio do círculo e uma string com atributos de estilo
+svgCircle :: Circle -> String -> String 
+svgCircle ((x,y),r) style = 
+  printf "<circle cx='%.3f' cy='%.3f' r='%.3f' stroke='black' style='%s' />\n" x y r style
 
 -- String inicial do SVG
 svgBegin :: Float -> Float -> String
@@ -51,7 +80,6 @@ svgElements func elements styles = concat $ zipWith func elements styles
 --------------------------------------
 -- Funções geradoras de arquivo SVG --
 --------------------------------------
-
 genCase1 :: IO ()
 genCase1 = do
   writeFile "case1.svg" $ svgstrs
@@ -60,5 +88,15 @@ genCase1 = do
         rects = genRect colunas linhas
         palette = greenPalette colunas linhas
         colunas = 10
-        linhas = 5
-        (w,h) = (1500,1500) -- width,height da imagem SVG
+        linhas = 20
+        (w,h) = (1024,768) -- width,height da imagem SVG
+
+genCase2 :: IO ()
+genCase2 = do
+  writeFile "case2.svg" $ svgstrs
+  where svgstrs = svgBegin w h ++ svgfigs ++ svgEnd
+        svgfigs = svgElements svgCircle circles (map svgStyle palette)
+        circles = genCircle1 circulos
+        palette = orangePallete circulos
+        circulos = 20
+        (w,h) = (1024,768) -- width,height da imagem SVG

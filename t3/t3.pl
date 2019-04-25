27d1da606fe94db051cdae153e9572d84353b1d2
pedrobms <pedro.bilar.montero@gmail.com>
2019-04-25 09:01:15 -0300

    ex 6 ao 10 adicionados

--- t3/t3.pl
+++ t3/t3.pl
@@ -27,3 +27,44 @@ comment([],[]).
 comment([X|Xs],[Y|Ys]) :-
   string_concat(X, '%%', Y),
   comment(Xs, Ys).
+
+%6 -
+onlyEven([],[]).
+onlyEven([X|Xs],[Y|Ys]) :-
+  N1 is mod(X, 2),
+  N1 =:= 0,
+  Y is X,
+  onlyEven(Xs, Ys).
+
+onlyEven([_|Xs], L2) :-
+  onlyEven(Xs, L2).
+
+%7 -
+countdown(0, []).
+countdown(N, [X|Xs]) :-
+  N > 0,
+  X is N,
+  N1 is N-1,
+  countdown(N1, Xs).
+
+%8 -
+nRandoms(0,[]).
+nRandoms(N,[X|Xs]) :-
+  N > 0,
+  random(0, 100, X),
+  N1 is N-1,
+  nRandoms(N1, Xs).
+
+%9 -
+potN0(0, []).
+potN0(N, [X|Xs]) :-
+  N > 0,
+  X is 2^N,
+  N1 is N-1,
+  potN0(N1, Xs).
+
+%10 -
+zipmult([],[],[]).
+zipmult([X|Xs],[Y|Ys],[Z|Zs]) :-
+  Z is X*Y,
+  zipmult(Xs, Ys, Zs).
